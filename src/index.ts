dc97470a1a8942500d8d8897f5cc988e846855ca
Nakarin Kakanumporn <belizwp@live.com>
2021-08-10 16:17:53 +0700

    feat: implement sendToWaitingListApi & sendToDeadLetterQueue

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
+import axios from "axios"
 import { Kafka } from "kafkajs"
 import { v4 as uuidv4 } from "uuid"
 
+import { mockWaitingListApiRequest } from "./mocks"
+import { Patient } from "./models"
+import { WaitingListApiRequest } from "./models"
+
 const TOPIC = "patient.with-risk-score.main"
+const DEAD_LETTER_QUEUE_TOPIC = "patient.with-risk-score.dlq"
 const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
 const BROKER_LIST = ["localhost:9092"]
 const GROUP_ID = "test-group"
+
+const WAITING_LIST_API_URL = "http://localhost:8999/ps/patient/create"
 
 const kafka = new Kafka({
   clientId: CLIENT_ID,
   brokers: BROKER_LIST,
 })
 
+const producer = kafka.producer()
 const consumer = kafka.consumer({ groupId: GROUP_ID })
-
-type SampleModel = {
-  id: number
-  name: string
-}
 
 const run = async () => {
   // Consuming
@@ -33,11 +37,11 @@ const run = async () => {
       const rawMessage = message?.value?.toString() || ""
 
       try {
-        const data = parseData(rawMessage)
-        sendToWaitingListApi(data!)
+        const patientData = parseData(rawMessage)
+        await sendToWaitingListApi(patientData!)
       } catch (error) {
         console.error(error)
-        sendToDeadLetterQueue(rawMessage)
+        await sendToDeadLetterQueue(rawMessage)
       }
     },
   })
@@ -45,19 +49,26 @@ const run = async () => {
 
 run().catch(console.error)
 
-function parseData(message: string): SampleModel | undefined {
+function parseData(message: string): Patient | undefined {
   try {
-    const data: SampleModel = JSON.parse(message)
+    const data: Patient = JSON.parse(message)
     return data
   } catch (error) {
     return
   }
 }
 
-function sendToDeadLetterQueue(message: string): void {
-  // TODO: implement this
+async function sendToDeadLetterQueue(message: string): Promise<void> {
+  await producer.connect()
+  await producer.send({
+    topic: DEAD_LETTER_QUEUE_TOPIC,
+    messages: [{ value: message }],
+  })
 }
 
-function sendToWaitingListApi(data: SampleModel) {
-  // TODO: implement this
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function sendToWaitingListApi(_data: Patient): Promise<void> {
+  // TODO: remove mock and transform Patient data to fit api body
+  const requestData: WaitingListApiRequest = mockWaitingListApiRequest  
+  await axios.post(WAITING_LIST_API_URL, requestData)
 }
--- src/models.ts
+++ src/models.ts
@@ -11,3 +11,111 @@ type RiskScore = {
   readonly InclusionLabelType: string
   readonly TriageScore: string
 }
+
+enum GenderCode {
+  unknown = 0,
+  male = 1,
+  female = 2,
+  notApplicable = 9,
+}
+
+export type WaitingListApiRequest = {
+  cdPersonID?: string
+  cdPersonForeignID?: string
+  cdPersonPassportID?: string
+  cdPersonNationalityCode?: number
+  cdPersonNameTitleCode?: number
+  cdPersonFirstName?: string
+  cdPersonMiddleName?: string
+  cdPersonLastName?: string
+  cdPersonGenderCode?: GenderCode
+  cdPersonAge?: number
+  cdPersonBirthDate?: string
+  cdPersonPhone1: string
+  cdPersonPhone2?: string
+  cdPersonCustodialPhone1?: string
+  cdPersonCustodialPhone2?: string
+  cdPersonWeightMeasure?: number
+  cdPersonHeightMeasure?: number
+  cdPersonBMIMeasure?: number
+  emLaboratoryTestATK?: boolean
+  emLaboratoryTestRTPCR?: boolean
+  emLaboratoryTestDate?: string // "2021-08-09T00:00:00.000+07:00"
+  emPatientGotFavipiravir?: boolean
+  emPatientGotFavipiravirDate?: string // "2021-06-01"
+  emPatientCommitStatusCode: number
+  emPatientCommitTemperature?: number
+  emPatientCommitPulse?: number
+  emPatientCommitOxygenSaturation?: number
+  emPatientCommitOxygenSaturationPost?: number
+  emPatientCommitOxygenSaturationDiff?: number
+  emPatientCommitSystolic?: number
+  emPatientCommitDiastolic?: number
+  emPatientCommitInspirationRate?: number
+  emPatientPregnancyStatus?: boolean
+  emPatientPregnancyWeeks?: number
+  emPatientBedriddenStatus?: boolean
+  emPatientSymptomsText?: string
+  emPatientAllergyDrug?: string
+  emPatientAllergyFood?: string
+  emPatientFoodText?: string
+  emPatientSymptomsCL1?: boolean
+  emPatientSymptomsCL2?: boolean
+  emPatientSymptomsCL3?: boolean
+  emPatientSymptomsCL4?: boolean
+  emPatientSymptomsCL5?: boolean
+  emPatientSymptomsCL6?: boolean
+  emPatientSymptomsCL7?: boolean
+  emPatientSymptomsCL8?: boolean
+  emPatientSymptomsCL9?: boolean
+  emPatientSymptomsCL10?: boolean
+  emPatientSymptomsCL11?: boolean
+  emPatientSymptomsCL12?: boolean
+  emPatientSymptomsCL13?: boolean
+  emPatientSymptomsCL14?: boolean
+  emPatientDiseaseCD1?: boolean
+  emPatientDiseaseCD2?: boolean
+  emPatientDiseaseCD3?: boolean
+  emPatientDiseaseCD4?: boolean
+  emPatientDiseaseCD5?: boolean
+  emPatientDiseaseCD6?: boolean
+  emPatientDiseaseCD7?: boolean
+  emPatientDiseaseCD8?: boolean
+  emPatientDiseaseCD9?: boolean
+  emPatientDiseaseCD10?: boolean
+  emPatientDiseaseCD11?: boolean
+  emPatientDiseaseCD12?: boolean
+  emPatientDiseaseCD13?: boolean
+  emHICICode?: number
+  emHICITypeCode?: number
+  cdMedicalDoctorCode?: string
+  emPatientCheckInDate?: string // "2021-06-01T00:00:00.000+07:00"
+  emPatientCheckOutDate?: string // "2021-06-01T00:00:00.000+07:00"
+  crProvinceCode: string
+  crAmpurCode: string
+  crTumbolCode?: string
+  crMooCode?: string
+  crRoad?: string
+  crTrok?: string
+  crSoi?: string
+  crVillage?: string
+  crZoneCode?: number
+  crBuildingName?: string
+  crAddressText?: string
+  crGeographicCoordinateLatitude?: string
+  crGeographicCoordinateLongitude?: string
+  emPatientCommitDate?: string // "2021-06-01T00:00:00.000+07:00"
+  emPatientMovementDate?: string // "2021-06-01T00:00:00.000+07:00"
+  emPatientWaitingHours?: number
+  emSourceNumberCode?: number
+  emMoveToLocationCode?: string
+  emMoveToLocationTypeCode?: number
+  emMoveFromLocationCode?: string
+  emMoveFromLocationTypeCode?: number
+  emMoveToMethodCode?: number
+  cdOrganizationMedicalUnit?: number
+  hsPatientHospitalNumber?: string
+  hsPatientAdmissionNumber?: string
+  hsPatientHealthCoverage?: number
+  riskScore: RiskScore
+}
