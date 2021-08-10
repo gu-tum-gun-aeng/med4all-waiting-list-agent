8010dcb2c1ea4337c4cb5215af76c712738286ef
Nakarin Kakanumporn <belizwp@live.com>
2021-08-10 14:58:19 +0700

    feat: add basic kafa consumer connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
-export * from './lib/async';
-export * from './lib/number';
+import { Kafka } from "kafkajs"
+import { v4 as uuidv4 } from "uuid"
+
+const TOPIC = "patient.with-risk-score.main"
+const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
+const BROKER_LIST = ["localhost:9092"]
+const GROUP_ID = "test-group"
+
+const kafka = new Kafka({
+  clientId: CLIENT_ID,
+  brokers: BROKER_LIST,
+})
+
+const consumer = kafka.consumer({ groupId: GROUP_ID })
+
+const run = async () => {
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: TOPIC, fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message?.value?.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
