2c419107b67ec29adee1872a232141c9da79eab0
catfly-tawan-w <c.catfly@gmail.com>
2021-08-11 11:08:27 +0700

    refactor: extract messageQueue from index.ts
    Tawan, Bell

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,17 @@
 import axios from "axios"
-import { Kafka } from "kafkajs"
-import { v4 as uuidv4 } from "uuid"
+
+import messageQueue from "./messageQueue"
 
 const TOPIC = "patient.with-risk-score.main"
 const DEAD_LETTER_QUEUE_TOPIC = "patient.with-risk-score.dlq"
-const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
-const BROKER_LIST = ["localhost:9092"]
-const GROUP_ID = "test-group"
 
 const WAITING_LIST_API_URL = "http://localhost:8999/ps/patient/create"
 
-const kafka = new Kafka({
-  clientId: CLIENT_ID,
-  brokers: BROKER_LIST,
-})
-
-const producer = kafka.producer()
-const consumer = kafka.consumer({ groupId: GROUP_ID })
+messageQueue.init()
 
 const run = async () => {
-  // Consuming
-  await consumer.connect()
-  await consumer.subscribe({ topic: TOPIC, fromBeginning: true })
-
-  await consumer.run({
-    eachMessage: async ({ partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message?.value?.toString(),
-      })
-      
-      const rawMessage = message?.value?.toString() || ""
-      processMessage(rawMessage)
-    },
-  })
+  await messageQueue.consume(TOPIC, processMessage)
 }
-
 run().catch(console.error)
 
 export async function processMessage(message: string): Promise<void> {
@@ -49,11 +24,7 @@ export async function processMessage(message: string): Promise<void> {
 }
 
 async function sendToDeadLetterQueue(message: string): Promise<void> {
-  await producer.connect()
-  await producer.send({
-    topic: DEAD_LETTER_QUEUE_TOPIC,
-    messages: [{ value: message }],
-  })
+  messageQueue.publish(DEAD_LETTER_QUEUE_TOPIC, message)
 }
 
 async function sendToWaitingListApi(data: string): Promise<void> {
---
+++ src/messageQueue.ts
@@ -0,0 +1,56 @@
+import { Consumer, Kafka, Producer } from "kafkajs"
+import { v4 as uuidv4 } from "uuid"
+
+let kafka: Kafka
+let producer: Producer
+let consumer: Consumer
+
+const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
+const BROKER_LIST = ["localhost:9092"]
+const GROUP_ID = "test-group"
+
+const messageQueue = {
+  init: () => {
+    kafka = new Kafka({
+      clientId: CLIENT_ID,
+      brokers: BROKER_LIST,
+    })
+    producer = kafka.producer()
+    consumer = kafka.consumer({ groupId: GROUP_ID })
+  },
+  consume: async (
+    topic: string,
+    cb: (message: string) => Promise<void>
+  ) => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: topic, fromBeginning: true })
+
+    await consumer.run({
+      eachMessage: async ({ partition, message }) => {
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value?.toString(),
+        })
+
+        if (message.value === null) { return }        
+        
+        cb(message.value.toString())
+      },
+    })
+    // TODO: disconnect consumer here
+  },
+  publish: async (
+    topic: string,
+    message: string
+  ): Promise<void> => {
+    await producer.connect()
+    await producer.send({
+      topic,
+      messages: [{ value: message }],
+    })
+    await producer.disconnect()
+  }
+}
+
+export default messageQueue
